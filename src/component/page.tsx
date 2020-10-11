eb1d5dba58f374d53ce429151fd38eaee49c959d
Kyle Nelson <kyle.nelson@virtual7.de>
2020-10-12 01:05:15 +0200

    sidebarfix

--- src/component/page.tsx
+++ src/component/page.tsx
@@ -23,7 +23,7 @@ const Container = styled.div`
 	width: 100%;
 	height: 100%;
 	min-height: 100vh;
-	min-width: calc(100vw - ${props => props.theme.sidebarWidth}px);
+	min-width: calc(100vw - ${props => props.theme.sidebarWidth + 17}px);
 	margin-left: ${props => props.theme.sidebarWidth}px;
   
   .menu-check {
